b482df65de4d6d94422d2fdde0621d647ba650d0
Evan Light <evan@tripledogdare.net>
2013-11-15 18:25:10 -0500

    Closes #35
    Nope, there isn't much reason to keep an empty String value instead of null if its breaking some users. Though we run the risk of some folks doing empty string comparisons when they should be using something like #blank?.

--- lib/generators/acts_as_commentable_with_threading_migration/templates/migration.rb
+++ lib/generators/acts_as_commentable_with_threading_migration/templates/migration.rb
@@ -2,10 +2,10 @@ class ActsAsCommentableWithThreadingMigration < ActiveRecord::Migration
   def self.up
     create_table :comments, :force => true do |t|
       t.integer :commentable_id, :default => 0
-      t.string :commentable_type, :default => ""
-      t.string :title, :default => ""
-      t.text :body, :default => ""
-      t.string :subject, :default => ""
+      t.string :commentable_type
+      t.string :title
+      t.text :body
+      t.string :subject{}
       t.integer :user_id, :default => 0, :null => false
       t.integer :parent_id, :lft, :rgt
       t.timestamps
