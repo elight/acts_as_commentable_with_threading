ae13360f7ab2b7166c1d937238835398758777c4
Luay Bseiso <luay@buttercloud.com>
2014-03-25 17:26:18 +0200

    fix syntax error and remove user_id index in migration

--- lib/generators/acts_as_commentable_with_threading_migration/templates/migration.rb
+++ lib/generators/acts_as_commentable_with_threading_migration/templates/migration.rb
@@ -6,13 +6,12 @@ class ActsAsCommentableWithThreadingMigration < ActiveRecord::Migration
       t.string :title
       t.text :body
       t.string :subject
-      t.refrences :commenter, :polymorphic => true
+      t.references :commenter, :polymorphic => true
       t.integer :parent_id, :lft, :rgt
       t.boolean :highlight, :default => false
       t.timestamps
     end
     
-    add_index :comments, :user_id
     add_index :comments, [:commentable_id, :commentable_type]
     add_index :comments, [:commenter_id, :commenter_type]
   end
