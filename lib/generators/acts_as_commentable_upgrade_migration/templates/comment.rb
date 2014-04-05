f79cb1f07d9afe4015b8fdf4b5aa12937f5929ef
Luay Bseiso <luay@buttercloud.com>
2014-04-05 12:34:22 +0300

    change user to commenter for acts_as_commentable upgrade templates

---
+++ lib/generators/acts_as_commentable_upgrade_migration/templates/comment.rb
@@ -0,0 +1,48 @@
+class Comment < ActiveRecord::Base
+  acts_as_nested_set :scope => [:commentable_id, :commentable_type]
+
+  validates :body, :presence => true
+  validates :commenter, :presence => true
+
+  # NOTE: install the acts_as_votable plugin if you
+  # want commenter to vote on the quality of comments.
+  #acts_as_votable
+
+  belongs_to :commentable, :polymorphic => true
+
+  # NOTE: Comments belong to a commenter
+  belongs_to :commenter, :polymorphic => true
+
+  # Helper class method that allows you to build a comment
+  # by passing a commentable object, a commenter object, and comment text
+  # example in readme
+  def self.build_from(obj, commenter, comment)
+    new \
+      :commentable  => obj,
+      :body         => comment,
+      :commenter    => commenter
+  end
+
+  #helper method to check if a comment has children
+  def has_children?
+    self.children.any?
+  end
+
+  # Helper class method to lookup all comments assigned
+  # to all commentable types for a given commenter.
+  scope :find_comments_by_commenter, lambda { |commenter|
+    where(:commenter_id => commenter.id, :commenter_type => commenter.class.to_s).order('created_at DESC')
+  }
+
+  # Helper class method to look up all comments for
+  # commentable class name and commentable id.
+  scope :find_comments_for_commentable, lambda { |commentable_str, commentable_id|
+    where(:commentable_type => commentable_str.to_s, :commentable_id => commentable_id).order('created_at DESC')
+  }
+
+  # Helper class method to look up a commentable object
+  # given the commentable class name and id
+  def self.find_commentable(commentable_str, commentable_id)
+    commentable_str.constantize.find(commentable_id)
+  end
+end
--- lib/generators/acts_as_commentable_upgrade_migration/templates/migration.rb
+++ lib/generators/acts_as_commentable_upgrade_migration/templates/migration.rb
@@ -1,13 +1,21 @@
 class ActsAsCommentableUpgradeMigration < ActiveRecord::Migration
   def self.up
     rename_column :comments, :comment, :body
-    add_column :comments, :subject, :string
-    add_column :comments, :parent_id, :integer
-    add_column :comments, :lft, :integer
-    add_column :comments, :rgt, :integer
-    add_column :comments, :updated_at, :datetime
+    add_column    :comments, :subject, :string
+    add_column    :comments, :parent_id, :integer
+    add_column    :comments, :lft, :integer
+    add_column    :comments, :rgt, :integer
+    add_column    :comments, :updated_at, :datetime
+    add_column    :comments, :commenter_id, :integer
+    add_column    :comments, :commenter_type, :string
+    remove_column :comments, :updated_at
+    remove_column :comments, :user_id
+    remove_column :comments, :user_type
     
-    add_index :comments, :commentable_id
+
+    add_index     :comments, :commentable_id
+    add_index     :comments, :commenter_id
+    remove_index  :comments, :user_id
   end
   
   def self.down
@@ -17,7 +25,15 @@ class ActsAsCommentableUpgradeMigration < ActiveRecord::Migration
     remove_column :comments, :lft
     remove_column :comments, :rgt
     remove_column :comments, :updated_at
+    remove_column :comments, :commenter_id, :integer
+    remove_column :comments, :commenter_type, :string
     
-    remove_index :comments, :commentable_id
+    add_column    :comments, :user_id, :integer
+    add_column    :comments, :user_type, :string
+    
+    remove_index  :comments, :commenter_id
+    remove_index  :comments, :commentable_id
+   
+    add_index     :comments, :user_id
   end
 end
