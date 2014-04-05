e6804794d71cf3ec87bf08ccb5c246b5aa0981d2
Luay Bseiso <luay@buttercloud.com>
2014-04-05 12:37:45 +0300

    update comments on comment model for fresh install template

--- lib/generators/acts_as_commentable_with_threading_migration/templates/comment.rb
+++ lib/generators/acts_as_commentable_with_threading_migration/templates/comment.rb
@@ -5,16 +5,16 @@ class Comment < ActiveRecord::Base
   validates :commenter, :presence => true
 
   # NOTE: install the acts_as_votable plugin if you
-  # want user to vote on the quality of comments.
+  # want commenter to vote on the quality of comments.
   #acts_as_votable
 
   belongs_to :commentable, :polymorphic => true
 
-  # NOTE: Comments belong to a user
+  # NOTE: Comments belong to a commenter
   belongs_to :commenter, :polymorphic => true
 
   # Helper class method that allows you to build a comment
-  # by passing a commentable object, a user_id, and comment text
+  # by passing a commentable object, a commenter object, and comment text
   # example in readme
   def self.build_from(obj, commenter, comment)
     new \
@@ -29,7 +29,7 @@ class Comment < ActiveRecord::Base
   end
 
   # Helper class method to lookup all comments assigned
-  # to all commentable types for a given user.
+  # to all commentable types for a given commenter.
   scope :find_comments_by_commenter, lambda { |commenter|
     where(:commenter_id => commenter.id, :commenter_type => commenter.class.to_s).order('created_at DESC')
   }
