1fbd6e4c8c0c8cb31120040a0434170181f60499
Chris Bielinski <chris@shadow.io>
2014-06-26 14:41:24 -0700

    update migration and test schema to *not* set default values for ID columns

--- lib/generators/acts_as_commentable_with_threading_migration/templates/migration.rb
+++ lib/generators/acts_as_commentable_with_threading_migration/templates/migration.rb
@@ -1,20 +1,20 @@
 class ActsAsCommentableWithThreadingMigration < ActiveRecord::Migration
   def self.up
     create_table :comments, :force => true do |t|
-      t.integer :commentable_id, :default => 0
+      t.integer :commentable_id
       t.string :commentable_type
       t.string :title
       t.text :body
       t.string :subject
-      t.integer :user_id, :default => 0, :null => false
+      t.integer :user_id, :null => false
       t.integer :parent_id, :lft, :rgt
       t.timestamps
     end
-    
+
     add_index :comments, :user_id
     add_index :comments, [:commentable_id, :commentable_type]
   end
-  
+
   def self.down
     drop_table :comments
   end
