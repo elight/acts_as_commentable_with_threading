1947931d9656e1d3ca2aaf9335f77760e9fa6c8a
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2014-09-15 13:34:01 -0700

    Update dependencies so that we support Rails 4+ only.  Neatened up Gemfiles so they only contain required lines.  Updated CHANGELOG and moved to Markdown.  Fixed deprecation warnings in specs.  Removed Rails 3.x gemfiles and Travis entries.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ plugin_test_dir = File.dirname(__FILE__)
 ActiveRecord::Base.logger = Logger.new(File.join(plugin_test_dir, "debug.log"))
 
 ActiveRecord::Base.configurations = YAML::load_file(File.join(plugin_test_dir, "db", "database.yml"))
-ActiveRecord::Base.establish_connection(ENV["DB"] || "sqlite3mem")
+ActiveRecord::Base.establish_connection((ENV["DB"] || "sqlite3mem").to_sym)
 ActiveRecord::Migration.verbose = false
 load(File.join(plugin_test_dir, "db", "schema.rb"))
 
